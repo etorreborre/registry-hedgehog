5b9ac3eb6859f173aa60255caf7a14300e96e13e
etorreborre <eric.torreborre@symbiont.io>
2020-04-09 11:56:37 +0200

    test: added some tests for generating fresh ids

--- test/Test/Data/Registry/HedgehogSpec.hs
+++ test/Test/Data/Registry/HedgehogSpec.hs
@@ -1,27 +1,38 @@
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE PartialTypeSignatures      #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 {-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
 module Test.Data.Registry.HedgehogSpec where
 
+import           Control.Monad.Morph (hoist)
+import           Data.IORef
 import           Data.Registry
 import           Data.Registry.Hedgehog
-import qualified Data.Text                     as T (length, take, toUpper)
 import           Hedgehog.Internal.Gen         hiding (print)
 import           Hedgehog.Range
+import           Hedgehog.Gen as Gen
 import           Protolude                     hiding (list)
+import           System.IO.Unsafe
 import           Test.Data.Registry.Company
 import           Test.Data.Registry.Generators
 import           Test.Tasty.Hedgehogx
+import           Hedgehog.Internal.Seed as Seed (random)
+import           Hedgehog.Internal.Tree as Tree (NodeT (..), runTreeT)
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 -- * This specification shows the usage of several features of this library
 --   First of all you will notice that if you run `stack test`
 --   all the properties of this file will be grouped under the Test.Data.Registry.HedgehogSpec test group
 
 -- * DECLARING PROPERTIES
+
+genWord :: MonadIO m => GenT m Text
+genWord = do
+  ws <- T.lines <$> liftIO (T.readFile "/usr/share/dict/words")
+  Gen.element ws
 
 test_a_simple_test =
   test "make a simple assertion, tested only once" $
@@ -104,3 +115,87 @@ test_distinct_values =
    -- uncomment to check
    -- collect =<< departmentName <$> forallS @Department
    success
+
+
+test_ints_generator =
+  prop "we can generate ints" $ do
+   n <- forAllT distinctInt
+   n === n -- collect n
+
+test_fresh = minTestsOk 10000 $
+  prop "we can generate terms with fresh ids" $ do
+    -- let termGenerator = genTerm :: GenT (StateT Int Identity) Term
+    m <- forAll $ runStateGen genTerm
+    -- collect m
+    m === m
+
+genFixedText :: (MonadGen m) => m Text
+genFixedText = pure "xxx"
+
+genTerm :: (MonadGen m, Fresh m) => m Term
+genTerm = Gen.recursive Gen.choice [genValue genFixedText] [Gen.subtermM2 genTerm genTerm makeExp]
+
+genValue :: (MonadGen m, Fresh m) => m Text -> m Term
+genValue g = do
+  t <- g
+  makeValue t
+
+data Term =
+    Value Int Text
+  | Exp Int Term Term
+  deriving (Eq, Show)
+
+makeValue :: (Monad m, Fresh m) => Text -> m Term
+makeValue t = do
+  n <- fresh
+  pure (Value n t)
+
+makeExp :: (Monad m, Fresh m) => Term -> Term -> m Term
+makeExp t1 t2 = do
+  n <- fresh
+  pure (Exp n t1 t2)
+
+class Fresh m where
+  fresh :: m Int
+
+runFresh :: (Show a) => GenT (State Int) a -> PropertyT IO a
+runFresh = forAll . runStateGen
+
+runStateGen :: (Show a) => GenT (State Int) a -> Gen a
+runStateGen = hoist (pure . flip evalState 0  )
+
+instance Fresh (GenT (State Int)) where
+  fresh = do
+    n <- get
+    put (n + 1)
+    pure n
+
+-- | Sample GenT IO values
+sampleGenIO :: GenT IO a -> IO a
+sampleGenIO gen =
+    let
+      loop n =
+        if n <= 0 then
+          panic "Hedgehog.Gen.sample: too many discards, could not generate a sample"
+        else do
+          seed <- Seed.random
+          NodeT r _  <- runTreeT $ evalGenT 30 seed gen
+          case r of
+            Nothing ->
+              loop (n - 1)
+            Just a ->
+              pure a
+    in
+      loop (100 :: Int)
+
+{-# NOINLINE distinctInt #-}
+distinctInt :: GenIO Int
+distinctInt = unsafePerformIO $ do
+  ref <- newIORef (0 :: Int)
+  pure $ distinctIntGenerator ref
+
+distinctIntGenerator :: IORef Int -> GenIO Int
+distinctIntGenerator ref = do
+  i <- lift $ readIORef ref
+  lift $ writeIORef ref (i + 1)
+  pure i
